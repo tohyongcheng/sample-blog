6996b82341fd137a5d3ed5d342f07e56156296c9
Toh Yong Cheng <tohyongcheng@gmail.com>
2025-05-14 03:24:27 +0000

    add demo route

--- server.js
+++ server.js
@@ -96,5 +96,10 @@ app.post("/delete/:id", async (req, res) => {
     }
   });
 
+app.get('/demo', function(req, res) {
+  res.render('pages/demo');
+});
+
+
 // Tells the app which port to run on
 app.listen(8080);
